b196994419b2f2aa14f0c9698ba56f71e92518f8
Teemo <58159901+liqideqq@users.noreply.github.com>
2025-04-28 19:17:14 +0800

    Update apk_sign.c

--- kernel/apk_sign.c
+++ kernel/apk_sign.c
@@ -334,5 +334,5 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 bool ksu_is_manager_apk(char *path)
 {
-	return check_v2_signature(path);
+	return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH);
 }
